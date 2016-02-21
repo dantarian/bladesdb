8f397e338ff32a2e7fd6cb22221563a56b2ea482
Warren Jones <warren@pencethren.org>
2016-02-21 22:44:10 +0000

    Added test for posting messages.

--- features/pages/board_page.rb
+++ features/pages/board_page.rb
@@ -1,6 +1,14 @@
 class BoardPage < BladesDBPage
     PAGE_TITLE = BladesDBPage::PAGE_TITLE
 
+    def post_message(from: nil, containing_text: nil)
+        if containing_text
+            fill_in("Post message", with: containing_text)
+        end
+        click_on("Post")
+        self
+    end
+
     def check_for_message(from: nil, containing_text: nil, containing_link: nil, relating_to_game: nil)
         message_div = page.find("div.message")
         if from
@@ -12,5 +20,6 @@ class BoardPage < BladesDBPage
         if containing_link
             message_div.find("div.messagebody").has_link?(relating_to_game.title, :href => containing_link)
         end
+        self
     end
 end
