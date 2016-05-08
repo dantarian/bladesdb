3338c11e92e3f392f0eda1fe48e2823400af662e
Judith Owens <lacks_imagination@hotmail.com>
2016-05-08 14:03:33 +0100

    Started writing tests for MP declaration and adjustment. Fixes #106, #128, #133, #142.

--- features/page_models/monster_points_page.rb
+++ features/page_models/monster_points_page.rb
@@ -1,6 +1,6 @@
 class MonsterPointsPage < BladesDBPage
     PAGE_TITLE = BladesDBPage::PAGE_TITLE + BladesDBPage::PAGE_TITLE_CONNECTOR + "Monster Points"
-    
+
     def declare_monster_points(date, points)
       click_link "Declare Monster Points"
       fill_in "Declared on", with: date.to_formatted_s + '\t'
@@ -8,7 +8,7 @@ class MonsterPointsPage < BladesDBPage
       page.find("span.ui-dialog-title").click
       page.find_button("Declare").trigger("click")
     end
-    
+
     def edit_declaration(date, points)
       click_link "Update"
       fill_in "Declared on", with: date.to_formatted_s + '\t'
@@ -16,7 +16,7 @@ class MonsterPointsPage < BladesDBPage
       page.find("span.ui-dialog-title").click
       page.find_button("Update").trigger("click")
     end
-    
+
     def request_adjustment(date, points, reason)
       click_link "Request Monster Points Adjustment"
       fill_in "Declared on", with: date.to_formatted_s + '\t'
@@ -36,7 +36,7 @@ class MonsterPointsPage < BladesDBPage
         row.find("td.points").should have_no_text(points.to_s)
       end
     end
-    
+
     def check_for_adjustment(points, display: true, state: "approved")
       page.should have_css("tr.provisional") if state == "provisional"
       page.should have_css("tr.rejected") if state == "rejected"
@@ -47,7 +47,7 @@ class MonsterPointsPage < BladesDBPage
         row.find("td.points").should have_no_text(points.to_s)
       end
     end
-    
+
     def check_for_monster_points(points)
       page.find("div#sessionpanel").should have_text("You have " + points.to_s + " monster points.")
       page.find("p#total").should have_text("Current total: " + points.to_s)
