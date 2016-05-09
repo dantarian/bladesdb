af69cd5900a821c5260190e84e8ce94e5d9b5089
Judith Owens <lacks_imagination@hotmail.com>
2016-05-09 22:07:28 +0100

    Tests written and passing for monster point adjustments.

--- features/page_models/monster_points_page.rb
+++ features/page_models/monster_points_page.rb
@@ -19,11 +19,12 @@ class MonsterPointsPage < BladesDBPage
 
     def request_adjustment(date, points, reason)
       click_link "Request Monster Points Adjustment"
-      fill_in "Declared on", with: date.to_formatted_s + '\t'
+      fill_in "Declared on", with: date.to_formatted_s
       fill_in I18n.t("user.monster_point_adjustment.points_label"), with: points
       fill_in "Reason", with: reason
       page.find("span.ui-dialog-title").click
-      page.find_button("Request").trigger("click")
+      sleep(1)
+      click_button "Request"
     end
 
     def check_for_declaration(points, display: true, state: "approved")
