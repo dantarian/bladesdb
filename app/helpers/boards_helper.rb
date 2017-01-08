8d2c478760fc3068c009ff12032192e3f8c9d176
Warren Jones <warren@pencethren.org>
2017-01-08 23:34:03 +0000

    Fixed problem with boards not displaying.

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -6,10 +6,10 @@ module BoardsHelper
       logger.warn("[BoardsHelper#number_of_messages] board is nil")
       0
     when board.messages.nil?
-      logger.warn("[BoardsHelper#number_of_messages] board.messages is nil")      
+      logger.warn("[BoardsHelper#number_of_messages] board.messages is nil")
       0
-    when board.messages.respond_to(:size) then board.messages.size
-    else 
+    when board.messages.respond_to?(:size) then board.messages.size
+    else
       logger.warn("[BoardsHelper#number_of_messages] board.messages does not respond to size()")
       0
     end
