13996c8a05fb849f8d338605fe91a12e640caee7
Warren Jones <warren@pencethren.org>
2017-07-16 15:12:31 +0100

    Fixed monster point tests broken by updated datepickers.

--- features/page_models/blades_db_page.rb
+++ features/page_models/blades_db_page.rb
@@ -8,19 +8,26 @@ class BladesDBPage
         visit url
         self
     end
-    
+
     def and
         self
     end
-    
+
     def log_out
       page.click_link("Log out")
     end
-    
+
+    ##
+    # Sets the date on a datepicker field, bypassing any protections provided by the UI.
+    def set_datepicker_date(id, date)
+      page.find("##{id}").send_keys '\t'
+      execute_script("$('##{id}').val('#{date.to_formatted_s}')")
+    end
+
     def check_is_displaying_message(message)
         page.should have_content(message)
     end
-    
+
     def check_for_links(text:, display: true)
         if display
           page.should have_link(text)
@@ -28,7 +35,7 @@ class BladesDBPage
           page.should have_no_link(text)
         end
     end
-    
+
     def check_for_table(table:, display: true)
         css = 'table#' + table
         if display
@@ -37,19 +44,18 @@ class BladesDBPage
           page.should have_no_css(css)
         end
     end
-    
+
     def check_pm_ratio(user)
         page.should have_selector("div#sessionpanel")
         page.find("div#sessionpanel").should have_text("Your current P:M ratio is")
     end
-  
+
     def check_monster_points(points)
         page.should have_selector("span#current_monster_points")
         page.find("span#current_monster_points").should have_text("#{points}")
     end
-    
+
     def check_error_message(message)
         page.find("div.errorExplanation, div.error").should have_text(message)
     end
 end
-
--- features/page_models/character_page.rb
+++ features/page_models/character_page.rb
@@ -5,7 +5,7 @@ class CharacterPage < BladesDBPage
 
     def buy_character_points_with_monster_points(character_points, date: Date.today)
         page.click_link "Spend monster points"
-        page.fill_in("Spent on", :with => date.strftime())
+        set_datepicker_date("monster_point_spend_spent_on", date)
         page.click_button "Next"
         page.fill_in("Character Points to buy", :with => character_points)
         page.click_button "Spend Points"
@@ -13,7 +13,7 @@ class CharacterPage < BladesDBPage
 
     def try_to_spend_monster_points_on(date)
         page.click_link "Spend monster points"
-        page.fill_in("Spent on", :with => date.strftime())
+        set_datepicker_date("monster_point_spend_spent_on", date)
         page.click_button "Next"
     end
 
