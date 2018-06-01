ad1ed99f55384da7e853a974402651b9dde66155
Warren Jones <warren@pencethren.org>
2018-06-01 23:41:08 +0100

    Added page test helper for Acceptables.

---
+++ features/page_models/acceptable_page.rb
@@ -0,0 +1,16 @@
+class AcceptablePage < BladesDBPage
+    PAGE_TITLE = BladesDBPage::PAGE_TITLE
+
+    def accept
+      click_on "Accept"
+    end
+
+    def reject
+      click_on "Reject"
+    end
+
+    def confirm_reject
+      click_on "Reject and suspend my account"
+    end
+
+end
